779280aad3610cecff5ba5b25f3f3b4eb9cdd210
phisit2744 <pisitstamp@gmail.com>
2024-06-17 22:29:25 +0700

    ok

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/",(req,res)=> {
-    res.send('Hello Go');
-})
+app.get("/", (req, res) => {
+    res.send('Hello World');
+});
 
-app.listen(port,()=>{
-    console.log("Listening on",port );
-})
+app.listen(port, () => {
+    console.log("Listening on port", port);
+});
